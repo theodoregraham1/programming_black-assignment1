cad44941b065a08ab171394ea14dcb994a32ae5b
theodoregraham1 <theodoregraham000@gmail.com>
2025-11-24 12:10:43 +0000

    Do work on server-side. It now returns files when requested (through node). Note: this is currently done in an incredibly secure way.Later need to use a static folder

--- server.js
+++ server.js
@@ -1,1 +1,41 @@
 "use strict";
+
+const http = require("node:http");
+const fs = require("node:fs");
+
+const hostname = "127.0.0.1";
+const port = 8080;
+
+const fileRegex = /\/\w+\.\w+/;
+
+const server = http.createServer((req, res) => {
+
+    req.on("error", (err) => {
+        console.error(err);
+    })
+    res.on("error", (err) => {
+        console.error(err);
+    })
+
+    const { url, method, headers } = req;
+
+    if (fileRegex.test(url)) {
+        // FIXME: this assumes all files are in the same directory and gives access to ALL files in that directory
+        res.statusCode = 200;
+        res.setHeader("content-type", "html");
+
+        const readStream = fs.createReadStream(`.${url}`);
+        let fileData = []
+        readStream
+            .on("data", (chunk) => {
+                res.write(chunk.toString());
+            })
+            .on("end", () => {
+                res.end();
+            });
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`http://${hostname}:${port}/index.html`)
+})
